24ac09d5dcd4a29d1b62a4916fb17ea3289167a0
Pitarn Harnpanichpun <pitarnh@outlook.com>
2016-06-09 17:27:55 +0200

    minor change

--- app/src/main/java/com/latte/oeuff/suicidepreventionapp/MainActivity.java
+++ app/src/main/java/com/latte/oeuff/suicidepreventionapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,8 +18,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener { //Listener for handling events on navigation items
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {     //https://developer.android.com/training/implementing-navigation/nav-drawer.html
@@ -28,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //Toolbar (Top)
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar); //Set a Toolbar to act as  ActionBar for this Activity
+
 
         // top-level container of "Navigation Drawer" (side)
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -40,8 +47,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
         //floating button (bottom)
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabBtn);
+        fab.setImageResource(R.drawable.ic_warning_black_40dp);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
