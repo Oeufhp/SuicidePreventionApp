5083465c46ae8525aedec6bc64ed66d785f6bdfd
Pitarn Harnpanichpun <pitarnh@outlook.com>
2016-06-10 16:56:15 +0200

    create reminder dialog first version

--- app/src/main/java/com/latte/oeuff/suicidepreventionapp/MainActivity.java
+++ app/src/main/java/com/latte/oeuff/suicidepreventionapp/MainActivity.java
@@ -3,15 +3,9 @@ package com.latte.oeuff.suicidepreventionapp;
 
 import android.content.Intent;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -21,16 +15,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener { //Listener for handling events on navigation items
 
 
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {            //https://developer.android.com/training/implementing-navigation/nav-drawer.html
+    //protected void onCreate(Bundle savedInstanceState) {            //https://developer.android.com/training/implementing-navigation/nav-drawer.html
                                                                     //https://developer.android.com/guide/topics/ui/menus.html
     protected void onCreate(Bundle savedInstanceState) {     //https://developer.android.com/training/implementing-navigation/nav-drawer.html
 
@@ -56,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         //floating button (bottom)
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabBtn);
-        fab.setImageResource(R.drawable.ic_warning_black_40dp);
+        fab.setImageResource(R.drawable.ic_warning_white_40dp);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/latte/oeuff/suicidepreventionapp/Reminders.java
+++ app/src/main/java/com/latte/oeuff/suicidepreventionapp/Reminders.java
@@ -2,14 +2,38 @@
 
 package com.latte.oeuff.suicidepreventionapp;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class Reminders extends AppCompatActivity {
+
+    private EditText fromDateEtxt;
+    private EditText toDateEtxt;
+
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
+
+    private SimpleDateFormat dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +42,29 @@ public class Reminders extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabBtnRem);
+        fab.setImageResource(R.drawable.ic_new_reminder);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Log.d("AA","BB");
+                final AlertDialog.Builder builder = new AlertDialog.Builder(Reminders.this);
+                LayoutInflater inflator = Reminders.this.getLayoutInflater();
+                builder.setView(inflator.inflate(R.layout.dialog_create_reminder, null));
+//                builder.setTitle("New Reminder");
+                builder.setPositiveButton("create reminder", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                builder.show();
+
             }
         });
     }
